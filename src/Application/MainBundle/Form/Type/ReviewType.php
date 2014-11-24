e85ee90660085e8c3338f3ea3c5b1d7e32412c6e
supertanuki <supertanuki@gmail.com>
2014-11-25 00:16:53 +0100

    add another dish

--- src/Application/MainBundle/Form/Type/ReviewType.php
+++ src/Application/MainBundle/Form/Type/ReviewType.php
@@ -13,14 +13,14 @@ class ReviewType extends AbstractType
         $builder
             ->add('review')
             ->add('rank')
-            ->add('when')
-            ->add('photoFile', 'vich_file', array(
-                'required'      => false,
-                'mapping'       => 'dish_photo',
-                'allow_delete'  => false,
-                'download_link' => false,
-            ))
-//            ->add('photoFile', 'file', array('label' => 'Photo'))
+            ->add('when', null, array('widget' => 'single_text'))
+//            ->add('photoFile', 'vich_file', array(
+//                'required'      => false,
+//                'mapping'       => 'dish_photo',
+//                'allow_delete'  => false,
+//                'download_link' => false,
+//            ))
+            ->add('photoFile', 'file', array('label' => 'Photo'))
         ;
     }
 
