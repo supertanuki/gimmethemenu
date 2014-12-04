ab3587360a79891d001fc4b47ee09d5f5fa2e8a2
supertanuki <supertanuki@gmail.com>
2014-12-04 01:04:48 +0100

    umask

--- web/app.php
+++ web/app.php
@@ -11,7 +11,9 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-//umask(0000);
+// If you don't want to setup permissions the proper way, just uncomment the following PHP line
+// read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
+umask(0002);
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
