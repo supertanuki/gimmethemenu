5ce3514d88391b9324da82c79b5c74562eab9657
supertanuki <supertanuki@gmail.com>
2014-12-12 15:22:22 +0100

    submit first item when press enter on map search

--- src/Application/MainBundle/Resources/public/js/mapsearch.js
+++ src/Application/MainBundle/Resources/public/js/mapsearch.js
@@ -51,6 +51,36 @@ $(document).ready(function() {
 
 
 
+        // store the original event binding function
+        var _addEventListener = (input.addEventListener) ? input.addEventListener : input.attachEvent;
+
+        function addEventListenerWrapper(type, listener) {
+            // Simulate a 'down arrow' keypress on hitting 'return' when no pac suggestion is selected,
+            // and then trigger the original listener.
+            if (type == "keydown") {
+                var orig_listener = listener;
+                listener = function(event) {
+                    var suggestion_selected = $(".pac-item-selected").length > 0;
+                    if (event.which == 13 && !suggestion_selected) {
+                        var simulated_downarrow = $.Event("keydown", {
+                            keyCode: 40,
+                            which: 40
+                        });
+                        orig_listener.apply(input, [simulated_downarrow]);
+                        input.blur();
+                    }
+
+                    orig_listener.apply(input, [event]);
+                };
+            }
+
+            _addEventListener.apply(input, [type, listener]);
+        }
+
+        input.addEventListener = addEventListenerWrapper;
+        input.attachEvent = addEventListenerWrapper;
+
+
         google.maps.event.addListener(autocomplete, 'place_changed', function() {
 
             infowindow.close();
