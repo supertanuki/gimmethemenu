3e619ed8f831736764fffaec2646ea6e271bd08f
supertanuki <supertanuki@gmail.com>
2015-01-02 22:51:51 +0100

    submenu for user profile

--- src/Application/MainBundle/Form/Type/ReviewType.php
+++ src/Application/MainBundle/Form/Type/ReviewType.php
@@ -17,12 +17,11 @@ class ReviewType extends AbstractType
             ))
             ->add('review', null, array(
                 'label' => 'Your review',
-                'attr' => array('rows' => 8)
+                'required' => true,
             ))
             ->add('personalNote', null, array(
                 'label' => 'Your personal note',
                 'required' => false,
-                'attr' => array('rows' => 5)
             ))
             ->add('when', null, array(
                 'widget' => 'single_text',
