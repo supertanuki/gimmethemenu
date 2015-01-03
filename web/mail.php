6c864c5b013de2aba7614e544d559e913f174706
supertanuki <supertanuki@gmail.com>
2015-01-03 21:36:07 +0100

    test email

---
+++ web/mail.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!isset($_SERVER['HTTP_HOST'])) {
+    exit('This script cannot be run from the CLI. Run it from a browser.');
+}
+
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+    '127.0.0.1',
+    '::1',
+    '128.79.236.237'
+))) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('This script is only accessible from localhost.');
+}
+
+if (mail('supertanuki@gmail.com', 'hello', 'hello', 'From: no-reply@gimmethemenu.com')) {
+    echo 'OK';
+} else {
+    echo 'Not working';
+}
